33a9a27ac9ff00ccd549702dccbe123476a3f39b
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-10-28 00:04:59 +0530

    food app

--- Month-3/week-3/oct-19/food/components/navbar.js
+++ Month-3/week-3/oct-19/food/components/navbar.js
@@ -2,9 +2,9 @@ function navbar(){
     return `<h3><a href="index.html">Home</a></h3>
 
     <div id="options">
-      <input placeholder= "receipe by name" />
-      <h3><a href="electronics.html">get receipe of the day</a></h3>
-      <h3><a href="electronics.html">show latest receipe</a></h3>
+      <h3><a href="search.html">search page</a></h3>
+      <h3><a href="getReceipe.html">get receipe of the day</a></h3>
+      <h3><a href="latestReceipe.html">show latest receipe</a></h3>
       <h3><a href="#">Sign In</a></h3>
       <h3><a href="#">Sign Up</a></h3>
     </div>`
