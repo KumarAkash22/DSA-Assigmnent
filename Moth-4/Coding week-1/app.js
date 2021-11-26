a5f30de03a0320c44e94858c33473d14188c4bd4
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-11-26 18:50:33 +0530

    usersCollevtion Coding assignment

--- Moth-4/Coding week-1/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/",(req,res)=>{
-    res.send({name:"Akash"})
-});
-app.listen(2345,function(){
-    console.log("listen on port 2345")
-});
