bde9d3bcb1e5d02be486e59d89ddb0eaf226c60b
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-11-29 15:10:05 +0530

    update

--- Moth-4/evaluation unit-4/coding-1/app.js
+++ Moth-4/evaluation unit-4/coding-1/app.js
@@ -5,7 +5,22 @@ const connect = () => {
     return mongoose.connect("mongodb://127.0.0.1:27017/naukri");
 }
 
-
+const companySchema = new mongoose.Schema(
+    {
+        company_name : {type: String, required:true},
+        company_address : {type: String, require: true},
+        company_type : {type:String, require:true},
+        company_foundation:{type:Number, require:false},
+        company_totalEmploy:{type:Number,require:false},
+        company_rating: {type: Number,required:true},
+        company_openjobs:{type:Number,required:true}
+    },
+    {
+        versionKey:false,
+        timestamps: true
+    }
+);
+const Company = mongoose.model("company", companySchema);
 const app = express();
 app.use(express.json());
 
