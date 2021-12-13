049298685e109a53d0999b21b87fa550987bdbe8
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-12-13 17:25:43 +0530

    update

--- Moth-4/evaluation unit-4/coding_2/src/detail.js
+++ Moth-4/evaluation unit-4/coding_2/src/detail.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const connect  = require("./configs/db");
+
+const authController = require("./controllers/auth.controller")
+// const emailController =require("./controllers/email.controller") 
+const movieController =require("./controllers/movie.controller") 
+const seatController = require("./controllers/seat.controller")
+const showController = require("./controllers/show.controller")
 const app = express();
 app.use(express.json());
+
+app.use("/movies",movieController);
+app.use("/shows",showController);
+app.use("/seats",showController);
+app.use("/shows",showController);
+app.use("/movies",movieController);
+app.use("/seat",seatController);
+app.use("/register",authController);
+app.use("/login",authController)
+
+
+
 app.listen(2345, async function(){
     await connect();
     console.log("listening on port 2345")
