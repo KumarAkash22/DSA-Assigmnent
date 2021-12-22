9bce8ac354f36b0e7992d9f09d4139bf66bb4244
Akash Kumar Prasad <akash22gupta33@gmail.com>
2021-12-22 21:02:22 +0530

    dpquestion

---
+++ Month-5/dpRobbingHouse1.js
@@ -0,0 +1,56 @@
+function robber(N,arr){
+    if(N == 1){
+        return arr[0];
+    }
+    if(N == 2){
+        return Math.max(arr[0],arr[1]);
+    }
+    arr[1] = Math.max(arr[0],arr[1])
+    for(var i = 2; i < N; i++){
+
+        arr[i] = Math.max(arr[i] + arr[i-2], arr[i-1] );
+        // console.log(arr[i])
+    }
+    return arr[N-1];
+}
+function runProgram(input) {
+    // Write code here
+    input = input.split("\n");
+    var testcases = +input[0];
+    var line = 1;
+    for(var i = 0; i < testcases; i++){
+        var N = +input[line++].trim();
+        var arr = input[line++].trim().split(" ").map(Number);
+        console.log(robber(N,arr));
+
+    }
+   
+  }
+  if (process.env.USERNAME === "admin") {
+    runProgram(`3
+    2
+    1 100
+    3
+    2 100 99
+    4
+    100 1 1 100`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
+  
+  
